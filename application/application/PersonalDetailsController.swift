233867cbf147f34019098e2003033c0623910f2e
dogaegee <dinhanli18@ku.edu.tr>
2023-04-08 22:53:53 +0300

    personal details page added without database connection

---
+++ application/application/PersonalDetailsController.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalDetailsController.swift
+//  application
+//
+//  Created by Doga Ege Inhanli on 8.04.2023.
+//
+
+import UIKit
+import SQLite
+
+class PersonalDetailsController: UIViewController {
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
