5bb9ab031b6c87d0bd14dce2f247e88f5b19312b
dogaegee <dinhanli18@ku.edu.tr>
2023-04-15 19:27:24 +0300

    qr code generation

--- passengerApp/passengerApp/QRCodePageController.swift
+++ passengerApp/passengerApp/QRCodePageController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 import PassKit
+import SQLite
+import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 class QRCodePageController: UIViewController {
     
+    
+    @IBOutlet weak var qrCodeImageView: UIImageView!
+    let filter = CIFilter.qrCodeGenerator()
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         showAnimate()
+        let str = "dskds"
+        generateQRCode(Data: str)
+        
+        
     }
+    
+    func generateQRCode(Data:String){
+        let data = Data.data(using: String.Encoding.ascii, allowLossyConversion: false)
+        
+        filter.setValue(data, forKey: "InputMessage")
+        
+        let ciImage = filter.outputImage
+        
+        let transform = CGAffineTransform(scaleX: 10, y: 10)
+        let transformImage = ciImage?.transformed(by: transform)
+        
+        let image = UIImage(ciImage: transformImage!)
+        qrCodeImageView.image = image
+    }
+    
     @IBAction func closeClicked(_ sender: UIButton) {
         removeAnimate()
     }
