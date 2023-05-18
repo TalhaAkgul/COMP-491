d4ec11d359e2d6d25accf5cb52a3e47bc7bfe699
TalhaAkgul <aakgul18@ku.edu.tr>
2023-05-19 02:07:12 +0300

    Bank Service Implemented

---
+++ OdeProServer/src/main/java/com/Softwaring/OdeProServer/dto/BankDTO.java
@@ -0,0 +1,23 @@
+package com.Softwaring.OdeProServer.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BankDTO {
+    String creditCardNo;
+    String cardHolder;
+    String expiration;
+    String cvc;
+
+    @Override
+    public String toString() {
+        return String.format("creditCardNo=%s, cardHolder=%s, expiration=%s, cvc=%s",
+                creditCardNo, cardHolder, expiration, cvc);
+    }
+}
--- OdeProServer/src/main/java/com/Softwaring/OdeProServer/entity/Bank.java
+++ OdeProServer/src/main/java/com/Softwaring/OdeProServer/entity/Bank.java
@@ -1,13 +1,36 @@
 package com.Softwaring.OdeProServer.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
-@Table
+@Table(name = "bank")
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class Bank {
+    @Id
+    @Column(name = "unique_card_id", nullable = false)
+    private String uniqueCardId;
+
+    @Column(name = "credit_card_no", length = 16, nullable = false)
+    private String creditCardNo;
+
+    @Column(name = "card_holder", nullable = false)
+    private String cardHolder;
+
+    @Column(name = "credit_limit", nullable = false)
+    private double creditLimit;
+
+    @Column(name = "expiration", length = 5, nullable = false)
+    private String expiration;
+
+    @Column(name = "cvc", length = 3, nullable = false)
+    private String cvc;
 }
