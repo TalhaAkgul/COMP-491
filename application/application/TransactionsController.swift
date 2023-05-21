fdc2565fd72013d6df17d7f1c0d5b68c9fff4844
dogaegee <dinhanli18@ku.edu.tr>
2023-05-22 02:09:59 +0300

    see all transactions page implemented

--- application/application/TransactionsController.swift
+++ application/application/TransactionsController.swift
@@ -9,7 +9,8 @@ import SQLite
 
 class TransactionsController: UIViewController {
     
-    @IBOutlet weak var transactionText: UITextView!
+
+    @IBOutlet weak var scrollView: UIScrollView!
     
     let databaseController = DatabaseController.instance
 
@@ -19,6 +20,7 @@ class TransactionsController: UIViewController {
         
         let screenSize: CGRect = UIScreen.main.bounds
         let screenHeight = screenSize.height
+        let screenWidth = screenSize.width
         let navigationItem = UINavigationItem(title: "Transactions")
         
         let back = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"),
@@ -30,23 +32,78 @@ class TransactionsController: UIViewController {
         let navigationBar = UINavigationBar(frame: CGRect(x: 0, y: screenHeight/25, width: view.frame.width, height: 44))
         navigationBar.barTintColor = UIColor(white: 0.95, alpha: 1.0)
         navigationBar.setItems([navigationItem], animated: false)
-
         view.addSubview(navigationBar)
-        var transactionStr = ""
-        do {
-            let selectQuery = databaseController.transactionTable.select(databaseController.transactionId, databaseController.amount, databaseController.passengerId)
-            let rows = try databaseController.database2.prepare(selectQuery)
-            for row in rows {
-                transactionStr += """
-                    transactionId: \(row[databaseController.transactionId]), \
-                    amount: \(row[databaseController.amount]), \
-                    passengerId: \(row[databaseController.passengerId])
-                """ + "\n"
+        
+        scrollView.frame.size.height = screenHeight * 0.8
+        scrollView.frame.size.width = screenWidth * 0.95
+        scrollView.center.x = self.view.center.x
+        scrollView.center.y = navigationBar.bounds.maxY + screenHeight/30 + scrollView.frame.height/2
+        let stackView = UIStackView()
+            stackView.axis = .vertical
+            stackView.spacing = 8
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            scrollView.addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+                stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
+                stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
+                stackView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 16),
+                stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+                stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -32)
+            ])
+        var transactionCount = 0
+            do {
+                let selectQuery = databaseController.transactionTable.select(databaseController.transactionId, databaseController.amount, databaseController.passengerId)
+                let rows = try databaseController.database2.prepare(selectQuery)
+                for row in rows {
+                    let transactionStackView = UIStackView()
+                    transactionStackView.axis = .vertical
+                    transactionStackView.spacing = 4
+                    transactionStackView.backgroundColor = .white
+                    
+                    let transactionIdLabel = UILabel()
+                    transactionIdLabel.font = UIFont.systemFont(ofSize: 14)
+                    transactionIdLabel.text = "Data and Time: \(row[databaseController.transactionId])"
+                    let amountLabel = UILabel()
+                    amountLabel.font = UIFont.systemFont(ofSize: 14)
+                    amountLabel.text = "Amount: \(row[databaseController.amount])"
+                    if(row[databaseController.amount] < 0){
+                        amountLabel.text! += " (Payment Refunded)"
+                    }
+                    let passengerIdLabel = UILabel()
+                    passengerIdLabel.font = UIFont.systemFont(ofSize: 14)
+                    passengerIdLabel.text = "Passenger ID: \(row[databaseController.passengerId])"
+
+                    [transactionIdLabel, amountLabel, passengerIdLabel].forEach { label in
+                            transactionStackView.addArrangedSubview(label)
+                    }
+
+                    stackView.addArrangedSubview(transactionStackView)
+                    transactionCount += 1
+                }
+            } catch {
+                print("Error selecting transactions from Transaction table: \(error)")
             }
-        } catch {
-            print("Error selecting transactions from Transaction table: \(error)")
-        }
-        transactionText.text = transactionStr
+        let countView = UIStackView()
+        countView.axis = .vertical
+        countView.spacing = 8
+        countView.translatesAutoresizingMaskIntoConstraints = false
+        countView.backgroundColor = .white
+        let countLabel = UILabel()
+        countLabel.font = UIFont.systemFont(ofSize: 14)
+        countLabel.text = "Number of Transactions: " + String(transactionCount)
+        countLabel.textAlignment = .center
+        countView.addArrangedSubview(countLabel)
+        
+        self.view.addSubview(countView)
+        NSLayoutConstraint.activate([
+            countView.topAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 16),
+            countView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            countView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+                countView.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+            countView.heightAnchor.constraint(equalToConstant: screenHeight / 20)
+
+        ])
     }
     
     @objc func goBack() {
@@ -54,6 +111,5 @@ class TransactionsController: UIViewController {
             viewController.modalPresentationStyle = .fullScreen
             present(viewController, animated: true, completion: nil)
         }
-
     }
 }
