8a494d05c02fc0deb0eb0d1613a2f0d4ce303cd7
dogaegee <dinhanli18@ku.edu.tr>
2023-05-24 14:17:14 +0300

    identity number captured from id card

--- application/application/IDViewController.swift
+++ application/application/IDViewController.swift
@@ -6,11 +6,94 @@
 //
 
 import UIKit
-
-class IDViewController: UIViewController {
-
+import AVFoundation
+import Vision
+class IDViewController: UIViewController,AVCaptureVideoDataOutputSampleBufferDelegate {
+    var captureSession: AVCaptureSession!
+        var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+        
+        // Vision variables
+        var textRecognitionRequest: VNRecognizeTextRequest!
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .green
+                self.view.backgroundColor = .green
+                captureSession = AVCaptureSession()
+                
+                guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
+                    fatalError("Failed to get the camera device")
+                }
+                
+                guard let videoInput = try? AVCaptureDeviceInput(device: videoCaptureDevice) else {
+                    fatalError("Failed to create the camera input")
+                }
+        setupVision()
+                captureSession.addInput(videoInput)
+                
+                let videoOutput = AVCaptureVideoDataOutput()
+                videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue.global(qos: .userInitiated))
+                captureSession.addOutput(videoOutput)
+                
+                captureSession.startRunning()
+                
+                videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                videoPreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+                videoPreviewLayer.frame = view.layer.bounds
+                view.layer.addSublayer(videoPreviewLayer)
+        
+
+          
     }
+    func setupVision() {
+            textRecognitionRequest = VNRecognizeTextRequest(completionHandler: { [weak self] request, error in
+                guard let observations = request.results as? [VNRecognizedTextObservation] else {
+                    return
+                }
+                //print(observations)
+                for observation in observations {
+                    guard let topCandidate = observation.topCandidates(1).first else { continue }
+                    
+                    // Process the recognized text here
+                    //let identityNumber = topCandidate.string
+                    //print("Identity Number: \(identityNumber)")
+                    // Process the recognized text here
+                                let text = topCandidate.string
+                                
+                                // Extract identity number
+                                if let identityNumber = self?.extractIdentityNumber(from: text) {
+                                    print("Identity Number: \(identityNumber)")
+                                }
+                }
+            })
+            
+            textRecognitionRequest.recognitionLevel = .accurate
+            textRecognitionRequest.usesLanguageCorrection = false
+        }
+    func extractIdentityNumber(from text: String) -> String? {
+        // Implement your logic here to extract the identity number
+        // You can use regular expressions, string manipulation, or other methods
+        
+        // Example: Extract the first 9-digit number
+        let regex = try! NSRegularExpression(pattern: "\\d{11}")
+        let matches = regex.matches(in: text, range: NSRange(text.startIndex..., in: text))
+        if let match = matches.first {
+            return String(text[Range(match.range, in: text)!])
+        }
+        
+        return nil
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+            guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+                return
+            }
+            
+            let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .right, options: [:])
+            
+            do {
+                try imageRequestHandler.perform([textRecognitionRequest])
+            } catch {
+                print("Error: \(error)")
+            }
+        }
+
 }
